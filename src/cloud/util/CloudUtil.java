73a09d45c9d73a2339ca5fa1254efb879dc44872
brandontm <brandon_btm@outlook.com>
2019-10-17 01:49:15 -0600

    Added HardDiskDrive Entity Changes to VirtualMachine

--- src/cloud/util/CloudUtil.java
+++ src/cloud/util/CloudUtil.java
@@ -3,50 +3,38 @@ package cloud.util;
 import java.util.Random;
 import java.util.UUID;
 
-import cloud.NetworkCard;
-import cloud.VirtualMachine;
-
 public class CloudUtil {
-	static int macElements = 6;
+    static int macElements = 6;
 
-	public static String getUUID() {
+    public static String getUUID(){
 		return UUID.randomUUID().toString();
 	}
 
-	public static String getMACAddress() {
-		/*
-		 * Método para generar MAC addresses aleatorias con fines educativos Basado en
-		 * método por Phate en Stack Overflow
-		 */
-		Random random = new Random();
-		byte[] macAddress = new byte[macElements];
+	public static String getMACAddress(){
+		/* Método para generar MAC addresses
+		 * aleatorias con fines educativos
+		 * Basado en método por Phate en 
+		 * Stack Overflow
+		 * */
+	    Random random = new Random();
+	    byte[] macAddress = new byte[macElements];
 
-		// Generar bytes aleatorios
-		random.nextBytes(macAddress);
+	    // Generar bytes aleatorios
+	    random.nextBytes(macAddress);
+	    
+	    // Crear un StringBuilder de 18 elementos
+	    // 64-D9-2C-A1-BD-B5
+	    StringBuilder mac = new StringBuilder(18);
 
-		// Crear un StringBuilder de 18 elementos
-		// 64-D9-2C-A1-BD-B5
-		StringBuilder mac = new StringBuilder(18);
+	    // Generar MAC Address basado en la generación
+	    // aleatoria
+    	int i = 0;
+	    for(byte b : macAddress){
+	    	mac.append(String.format("%02x%s", b, (mac.length() >= 0 && i < macElements -1 ? ":" : "")));
+	    	i++;
+	    }
 
-		// Generar MAC Address basado en la generación
-		// aleatoria
-		int i = 0;
-		for (byte b : macAddress) {
-			mac.append(String.format("%02x%s", b, (mac.length() >= 0 && i < macElements - 1 ? ":" : "")));
-			i++;
-		}
-
-		// Regresar la dirección MAC en mayúsculas
-		return mac.toString().toUpperCase();
-	}
-
-	public static void printNICInfo(NetworkCard nic) {
-		// Escribe tu código
-		System.out.println(nic.toString());
-	}
-
-	public static void printVMInfo(VirtualMachine vm) {
-		// Escribe tu código
-		System.out.println(vm.toString());
+	    // Regresar la dirección MAC en mayúsculas
+	    return mac.toString().toUpperCase();
 	}
 }
